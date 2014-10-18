1c1a76c69b12e223c178b5062a29e7b16271d19b
abradbury <abradbury@users.noreply.github.com>
2014-10-18 18:51:06 +0100

    Finished cohesion, alignment, separation functions

--- Prototype/toplevel.cpp
+++ Prototype/toplevel.cpp
@@ -2,10 +2,12 @@
 
 #define PARAMLEN 3
 #define DIMENSIONS 3
+
 #define MAXBOIDS 10
 #define MAXNEIGHBOURS 20
 
 #define VISIONRADIUS 3
+#define MAXSPEED 3
 
 // Class
 class Vector {
@@ -17,8 +19,14 @@ class Vector {
 		Vector();
 		Vector(uint8 x_, uint8 y_, uint8 z_);
 		void add(Vector v);
+		void sub(Vector v);
+		void mul(uint8 n);
+		void div(uint8 n);
 		uint8 mag();
+		void normalise();
+		void bound(uint8 n);
 
+		// FIXME: Not working
 		friend std::ostream& operator<<(std::ostream& os, const Vector& v);
 };
 
@@ -39,7 +47,7 @@ class Boid {
 		uint8 getNeighbourCount();
 
 		void updateVelocity(Vector newVelocity);
-		void updatePosition(Vector newPos);
+		void updatePosition(Vector newPosition);
 		void addNeighbour(uint8 neighbourID);
 
 		void printBoidInfo();
@@ -49,9 +57,9 @@ class Boid {
 void setupEnvironment(uint32 *data);
 void calcNeighbours(Boid* b);
 uint8 calcDistance(Boid* b1, Boid* b2);
-uint8 align(Boid* b);
-uint8 group(Boid* b);
-uint8 repel(Boid* b);
+Vector align(Boid* b);
+Vector group(Boid* b);
+Vector repel(Boid* b);
 
 // Parameter string
 uint32 paramData[PARAMLEN];
@@ -78,10 +86,40 @@ void Vector::add(Vector v) {
 	z = z + v.z;
 }
 
+void Vector::sub(Vector v) {
+	x = x - v.x;
+	y = y - v.y;
+	z = z - v.z;
+}
+
+void Vector::mul(uint8 n) {
+	x = x * n;
+	y = y * n;
+	z = z * n;
+}
+
+void Vector::div(uint8 n) {
+	if (n != 0) {
+		x = x / n;
+		y = y / n;
+		z = z / n;
+	}
+}
+
 uint8 Vector::mag() {
 	return sqrt(double(x*x + y*y + z*z));
 }
 
+void Vector::normalise() {
+	uint8 m = mag();
+	div(m);
+}
+
+void Vector::bound(uint8 n) {
+	//TODO: Bound the speed somehow
+}
+
+//FIXME: Not currently working
 std::ostream& operator<<(std::ostream& os, const Vector& v)
 {
     os << v.x << ', ' << v.y << ', ' << v.z;
@@ -131,10 +169,31 @@ void Boid::addNeighbour(uint8 neighbourID) {
 	nCount++;
 }
 
+void Boid::updateVelocity(Vector newVelocity) {
+	std::cout << "Boid " << id << " changed velocity from [" << velocity.x <<
+		", " << velocity.y << ", " << velocity.z << "] to [";
+
+	velocity.add(newVelocity);
+
+	std::cout << velocity.x << ", " << velocity.y << ", " << velocity.z <<
+		"]" << std::endl;
+}
+
+void Boid::updatePosition(Vector newPosition) {
+	std::cout << "Boid " << id << " moved from [" << position.x << ", " <<
+		position.y << ", " << position.z << "] to [";
+
+	position.add(newPosition);
+
+	std::cout << position.x << ", " << position.y << ", " << position.z <<
+		"]" << std::endl;
+}
+
 // Other ///////////////////////////////////////////////////////////////////////
 void Boid::printBoidInfo() {
 	std::cout << "==========Info for Boid " << id << "==========" << std::endl;
-	std::cout << "Boid Velocity: " << velocity << std::endl;
+	std::cout << "Boid Velocity: [" << velocity.x << ", " << velocity.y <<
+			", " << velocity.z << "]" << std::endl;
 	std::cout << "Boid Position: [" << position.x << ", " << position.y <<
 			", " << position.z << "]" << std::endl;
 	std::cout << "===================================" << std::endl;
@@ -156,25 +215,38 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 		paramData[i] = input.read();
 	}
 
+	// Setup the environment
 	setupEnvironment(paramData);
 	for(int i = 0; i < paramData[0]; i++) {
 		boidList[i]->printBoidInfo();
 	}
 
 	// While....
-	for(uint8 b = 0; b < MAXBOIDS; b++) {
-		// Calculate the modifications to the boid
-		Boid* bob = boidList[b];
-		calcNeighbours(bob);
+//	while(1) {
+		for(uint8 b = 0; b < MAXBOIDS; b++) {
+			// Calculate the boid's neighbours
+			Boid* bob = boidList[b];
+			calcNeighbours(bob);
 
-		uint8 alignMod = align(bob);
-		uint8 groupMod = group(bob);
-		uint8 repelMod = repel(bob);
+			// Calculate the modifications to the boid
+			Vector alignMod = align(bob);
+			Vector groupMod = group(bob);
+			Vector repelMod = repel(bob);
 
-		// Apply the modifications
-		// Add velocity to location
+			// Cap values and apply the modifications
+			Vector totalMod;
+			totalMod.add(alignMod);
+			totalMod.add(groupMod);
+			totalMod.add(repelMod);
 
-	}
+			totalMod.bound(MAXSPEED);
+
+			bob->updateVelocity(totalMod);
+
+//			totalMod.add(bob->getVelocity());
+//			bob->updatePosition(totalMod);
+		}
+//	}
 }
 
 void setupEnvironment(uint32 *data) {
@@ -189,16 +261,16 @@ void setupEnvironment(uint32 *data) {
 //		boidList[i]->printBoidInfo();
 //	}
 
-	boidList[0] = new Boid(Vector(2,13,0), Vector(2,13,0), 1);
-	boidList[1] = new Boid(Vector(6,12,0), Vector(2,13,0), 2);
-	boidList[2] = new Boid(Vector(5,10,0), Vector(2,13,0), 3);
-	boidList[3] = new Boid(Vector(9,8,0), Vector(2,13,0), 4);
-	boidList[4] = new Boid(Vector(8,7,0), Vector(2,13,0), 5);
-	boidList[5] = new Boid(Vector(7,5,0), Vector(2,13,0), 6);
-	boidList[6] = new Boid(Vector(11,6,0), Vector(2,13,0), 7);
-	boidList[7] = new Boid(Vector(10,5,0), Vector(2,13,0), 8);
-	boidList[8] = new Boid(Vector(11,4,0), Vector(2,13,0), 9);
-	boidList[9] = new Boid(Vector(4,3,0), Vector(2,13,0), 10);
+	boidList[0] = new Boid(Vector(2,13,0), Vector(1,1,0), 1);
+	boidList[1] = new Boid(Vector(6,12,0), Vector(0,1,0), 2);
+	boidList[2] = new Boid(Vector(5,10,0), Vector(1,0,0), 3);
+	boidList[3] = new Boid(Vector(9,8,0), Vector(2,0,0), 4);
+	boidList[4] = new Boid(Vector(8,7,0), Vector(2,2,0), 5);
+	boidList[5] = new Boid(Vector(7,5,0), Vector(2,1,0), 6);
+	boidList[6] = new Boid(Vector(11,6,0), Vector(1,0,0), 7);
+	boidList[7] = new Boid(Vector(10,5,0), Vector(1,2,0), 8);
+	boidList[8] = new Boid(Vector(11,4,0), Vector(1,2,0), 9);
+	boidList[9] = new Boid(Vector(4,3,0), Vector(1,1,0), 10);
 }
 
 void calcNeighbours(Boid* b) {
@@ -215,7 +287,7 @@ void calcNeighbours(Boid* b) {
 	}
 
 	// Display neighbouring boids
-	std::cout << "Boid " << b->getID() << " has neighbours ";
+	std::cout << "Boid " << b->getID() << " has neighbours: ";
 	for (int i = 0; i < b->getNeighbourCount(); i++) {
 		std::cout << b->getNeighbour(i) << ", ";
 	}
@@ -231,37 +303,58 @@ uint8 calcDistance(Boid* b1, Boid* b2) {
 	double zs = pow(double(b1->getPosition().z - b2->getPosition().z), 2);
 
 	double dist = sqrt(xs + ys + zs);
-	//std::cout << "Boids " << b1->getID() << " and " << b2->getID() << " are " << dist << " units apart " << std::endl;
 
 	return (uint8)(round(dist));
 }
 
 /**
- * Iterate through a boid's neighbours, summing their velocities
+ * Iterate through a boid's neighbours, summing their velocities. Then divide
+ * by the number of neighbours and normalise.
  */
-uint8 align(Boid* b) {
-	uint8 velocity = 0;
-//	for (int i = 0; i < b->getNeighbourCount(); i++) {
-//		velocity += boidList[b->getNeighbour(i)]->getVelocity();
-//	}
-//
-//	velocity /= b->getNeighbourCount();
-	//TODO: Normalise and use proper vector notation
-	return velocity;
+Vector align(Boid* b) {
+	Vector alignment;
+	for (int i = 0; i < b->getNeighbourCount(); i++) {
+		alignment.add(boidList[b->getNeighbour(i)]->getVelocity());
+	}
+
+	alignment.div(b->getNeighbourCount());
+	alignment.normalise();
+	return alignment;
 }
 
-uint8 group(Boid* b) {
-	uint8 position[DIMENSIONS] = {0, 0, 0};
-//	for (int i = 0; i < b->getNeighbourCount(); i++) {
-//		position[0] += boidList[b->getNeighbour(i)]->getX();
-//		position[1] += boidList[b->getNeighbour(i)]->getY();
-//		position[2] += boidList[b->getNeighbour(i)]->getZ();
-//	}
-	return 0;
+/**
+ * Cohesion: causes the boids to steer towards one another. Sum the position of
+ * all the neighbouring boids, divide by neighbour count, process and normalise.
+ */
+Vector group(Boid* b) {
+	Vector cohesion;
+	for (int i = 0; i < b->getNeighbourCount(); i++) {
+		cohesion.add(boidList[b->getNeighbour(i)]->getPosition());
+	}
+
+	cohesion.div(b->getNeighbourCount());
+	cohesion.sub(b->getPosition());
+	cohesion.normalise();
+	return cohesion;
 }
 
-uint8 repel(Boid* b) {
+/**
+ * Separation: causes the boid to steer away from its neighbours, i.e. it needs
+ * to be close, but not too close. The distance between neighbouring boids is
+ * added to the computation vector
+ */
+Vector repel(Boid* b) {
+	Vector separation;
+	Vector dist;
+	for (int i = 0; i < b->getNeighbourCount(); i++) {
+		dist = boidList[b->getNeighbour(i)]->getPosition();
+		dist.sub(b->getPosition());
+		separation.add(dist);
+	}
 
-	return 0;
+	separation.div(b->getNeighbourCount());
+	separation.mul(-1);
+	separation.normalise();
+	return separation;
 }
 
