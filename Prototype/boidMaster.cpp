52d28f07734effcabe1f630e1e22224e3b732508
abradbury <abradbury@users.noreply.github.com>
2015-04-28 08:45:07 +0100

    Restored load balancing code to BoidMaster

--- Prototype/boidMaster.cpp
+++ Prototype/boidMaster.cpp
@@ -1,8 +1,18 @@
 #include "boidMaster.h"
 
-//#define USING_TB				true	// Defined when using VHLS test bench
+//#define USING_TESTBENCH				true	// Define when using TestBench
+//#define LOAD_BALANCING_ENABLED		true	// Define to enable load balancing
+
+// TODO: Check with load balancing commented out
+// TODO: Add comments
 
 #define MAX_BOIDCPUS			32		// TODO: Decide on a suitable value
+#define MAX_GATEKEEPERS			16		// TODO: Decide on a suitable value
+
+#define NORTH_IDX	12			// The index of the north edge change (load bal)
+#define EAST_IDX	8			// The index of the east edge change (load bal)
+#define SOUTH_IDX	4			// The index of the south edge change (load bal)
+#define WEST_IDX	0			// The index of the west edge change (load bal)
 
 #define SIMULATION_WIDTH		1280	// The pixel width of the simulation
 #define SIMULATION_HEIGHT		720		// The pixel height of the simulation
@@ -18,11 +28,16 @@ void sendUserDataToBoidGPU();
 
 void issueCalcNbrsMode();
 void issueCalcBoidMode();
-//void issueLoadBalance(); // TODO
 void issueTransferMode();
 void issueDrawMode();
 
 void killSimulation();
+
+#ifdef LOAD_BALANCING_ENABLED
+void processLoadData();
+void issueLoadBalance();
+void updateMinimalBoidCPUsList();
+#endif
 
 void setupSimulation();
 void closestMultiples(uint12 *height, uint12 *width, uint8 number);
@@ -50,7 +65,22 @@ struct BoidCPU {
 	uint32 gatekeeperID;
 	uint8 x;
 	uint8 y;
+
+#ifdef LOAD_BALANCING_ENABLED
+	bool minimalHeight;
+	bool minimalWidth;
+#endif
 };
+
+#ifdef LOAD_BALANCING_ENABLED
+struct AckStruct {
+	uint32 gatekeeperID;
+	bool recieved;
+	bool loadBalancing;
+};
+
+AckStruct ackList[MAX_GATEKEEPERS];
+#endif
 
 uint8 state = CMD_PING;
 uint8 ackCount = 0;
@@ -58,6 +88,8 @@ uint8 gatekeeperCount = 0;
 uint8 boidCPUCount = 0;
 BoidCPU boidCPUs[MAX_BOIDCPUS];
 
+uint12 simulationGridHeight = 0;
+uint12 simulationGridWidth  = 0;
 uint8 gridAssignment[MAX_BOIDCPUS][MAX_BOIDCPUS];
 
 uint32 boidCount = 100;
@@ -71,14 +103,11 @@ void boidMaster(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 #pragma HLS RESOURCE variable = output core = AXI4Stream
 #pragma HLS INTERFACE ap_ctrl_none port = return
 
-	// Perform initialisation
-//    initialisation();
-
 // Continually check for input and deal with it. Note that reading an empty
 // input stream will generate warnings in HLS, but should be blocking in the
 // actual implementation.
 
-#ifdef USING_TB
+#ifdef USING_TESTBENCH
 	inputData[CMD_LEN] = input.read();
 #endif
 
@@ -113,10 +142,14 @@ void boidMaster(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 			case CMD_PING_END:
 				pingEnd = true;
 				break;
-//			case CMD_LOAD_BAL_REPLY:
-//				// TODO: Implement load balancing
-//				processLoadData();
-//				break;
+#ifdef LOAD_BALANCING_ENABLED
+			case CMD_LOAD_BAL_REQUEST:
+				processLoadData();
+				break;
+			case CMD_BOUNDS_AT_MIN:
+				updateMinimalBoidCPUsList();
+				break;
+#endif
 			case CMD_ACK:
 				processAck();
 				break;
@@ -143,7 +176,7 @@ void boidMaster(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 		outputCount = 0;
 		// ---------------------------------------------------------------------
 
-#ifdef USING_TB
+#ifdef USING_TESTBENCH
 		continueOperation = input.read_nb(inputData[0]);
 #endif
 	}
@@ -175,6 +208,10 @@ void processUserData() {
  * another matter), the simulation setup is calculated.
  */
 void processPingReply() {
+#ifdef LOAD_BALANCING_ENABLED
+	ackList[gatekeeperCount].gatekeeperID = inputData[CMD_FROM];
+#endif
+
 	uint8 gatekeeperBoidCPUCount = inputData[CMD_HEADER_LEN + 0];
 	gatekeeperCount++;
 
@@ -217,9 +254,6 @@ void setupSimulation() {
 	}
 
 	// Determine simulation grid layout
-	uint12 simulationGridHeight = 0;
-	uint12 simulationGridWidth  = 0;
-
 	closestMultiples(&simulationGridHeight, &simulationGridWidth, boidCPUCount);
 
 	std::cout << "Simulation is " << simulationGridWidth << " BoidCPUs wide by "
@@ -267,6 +301,20 @@ void setupSimulation() {
 				boidCPUs[count].boidCPUCoords[3] = boidCPUs[count].\
 						boidCPUCoords[1] + boidCPUPixelHeight;
 			}
+
+#ifdef LOAD_BALANCING_ENABLED
+			// Is minimal?
+			if ((boidCPUs[count].boidCPUCoords[2] - boidCPUs[count].\
+					boidCPUCoords[0]) <= VISION_RADIUS) {
+				boidCPUs[count].minimalHeight = true;
+			} else if ((boidCPUs[count].boidCPUCoords[3] - boidCPUs[count].\
+					boidCPUCoords[1]) <= VISION_RADIUS) {
+				boidCPUs[count].minimalWidth = true;
+			} else {
+				boidCPUs[count].minimalHeight = false;
+				boidCPUs[count].minimalWidth = false;
+			}
+#endif
 
 			// Store the grid position of the BoidCPU
 			boidCPUs[count].x = w;
@@ -355,6 +403,21 @@ void closestMultiples(uint12 *height, uint12 *width, uint8 number) {
 	}
 }
 
+/**
+ * Process a received ACK. Typically this moves the simulation on to the next
+ * stage. When an ACK is received mark that gatekeeper as having send its ACK
+ * and increment the ACK counter.
+ *
+ * In load balancing, BoidCPUs send an ACK if they do not need to load balance.
+ * A BoidCPU that does need to load balance will send a load balance request,
+ * not an ACK. During load balancing, other BoidCPUs may be affected. Every
+ * gatekeeper that has BoidCPUs affected by the load balancing has its ACK flag
+ * cleared and a load balancing flag set for it. That way, when an ACK from the
+ * original load balancing mode is received after the load balancing change
+ * messages have been received, this ACK is only counted if the gatekeeper it
+ * is from does not have any affected BoidCPUs. Otherwise, it is ignored. For
+ * gatekeepers that have affected BoidCPUs, another ACK will be sent.
+ */
 void processAck() {
 
 	if (inputData[CMD_FROM] == BOIDGPU_ID) {
@@ -363,8 +426,25 @@ void processAck() {
 		ackCount = 0;
 	} else {
 		ackCount++;
-	}
+#ifdef LOAD_BALANCING_ENABLED
+		for (int i = 0; i < gatekeeperCount; i++) {
+			if (ackList[i].gatekeeperID == inputData[CMD_FROM]) {
 
+				if (ackList[i].loadBalancing == true) {
+					if (inputData[CMD_HEADER_LEN] == CMD_LOAD_BAL) {
+						ackList[i].recieved = true;
+						ackCount++;
+					} else {
+						std::cout << "Ignored ACK (as load bal)" << std::endl;
+					}
+				} else {
+					ackList[i].recieved = true;
+					ackCount++;
+				}
+			}
+		}
+#endif
+	}
 	if (ackCount == gatekeeperCount) {
 		switch(state) {
 		case CMD_SIM_SETUP:
@@ -380,15 +460,186 @@ void processAck() {
 			issueTransferMode();
 			break;
 		case MODE_TRAN_BOIDS:
+#ifdef LOAD_BALANCING_ENABLED
+			state = MODE_LOAD_BAL;
+			issueLoadBalance();
+			break;
+#else
 			state = MODE_DRAW;
 			issueDrawMode();
 			break;
+#endif
 		default:
 			break;
 		}
 		ackCount = 0;
+
+#ifdef LOAD_BALANCING_ENABLED
+		for (int i = 0; i < gatekeeperCount; i++) {
+			ackList[i].recieved = false;
+			ackList[i].loadBalancing = false;
+		}
+#endif
 	}
 }
+
+#ifdef LOAD_BALANCING_ENABLED
+/**
+ * Calculate the boundary changes for the overloaded BoidCPU and the other
+ * BoidCPUs that are affected by this change. A step size is the same as the
+ * vision radius of a boid.
+ */
+void processLoadData() {
+	// TODO: Utilise the minimal BoidCPU information
+
+	// Get info about BoidCPU making request
+	uint8 x = boidCPUs[inputData[CMD_FROM] - FIRST_BOIDCPU_ID].x;
+	uint8 y = boidCPUs[inputData[CMD_FROM] - FIRST_BOIDCPU_ID].y;
+
+	// Determine changes for the BoidCPU that made the request
+	int16 edgeChanges = 0;
+	int4 stepChanges = 1;
+	std::cout << "Overloaded BoidCPU (#" << inputData[CMD_FROM] << ") [" <<
+			x << ", " <<  y << "]: ";
+
+	// If the BoidCPU is not on the topmost row of the simulation grid
+	if (y != 0) {
+		edgeChanges |= (int16(stepChanges) << NORTH_IDX);
+		std::cout << "NORTH edge decreased, ";
+	}
+
+	// If the BoidCPU is not on the rightmost column of the simulation grid
+	if (x != (simulationGridWidth - 1)) {
+		edgeChanges |= ((~(int16)0xF000) & (int16(-stepChanges) << EAST_IDX));
+		std::cout << "EAST edge decreased, ";
+	}
+
+	// If the BoidCPU is not on the bottom-most row of the simulation grid
+	if (y != (simulationGridHeight - 1)) {
+		edgeChanges |= ((~(int16)0xFF00) & (int16(-stepChanges) << SOUTH_IDX));
+		std::cout << "SOUTH edge decreased, ";
+	}
+
+	// If the BoidCPU is not on the leftmost column of the simulation gird
+	if (x != 0) {
+		edgeChanges |= ((~(int16)0xFFF0) & (int16(stepChanges) << WEST_IDX));
+		std::cout << "WEST edge decreased, ";
+	}
+	std::cout << std::endl;
+
+	// Determine changes for other, affected BoidCPUs
+	for (int i = 0; i < boidCPUCount; i++) {
+
+		int16 affectedBoidCPUEdgeChanges = 0;
+		std::cout << "BoidCPU #" << boidCPUs[i].boidCPUID << ": ";
+
+		// If the NORTH edge of the overloaded BoidCPU is changing and this
+		// BoidCPU is on the row above the overloaded one, lower this
+		// BoidCPU's SOUTH edge. If on same row lower its NORTH edge.
+		if (int4(edgeChanges >> NORTH_IDX) != 0) {
+			if (boidCPUs[i].y == (y - 1)) {
+				affectedBoidCPUEdgeChanges |= ((~(int16)0xFF00) &
+						(int16(stepChanges) << SOUTH_IDX));
+				std::cout << "SOUTH edge increased, ";
+			} else if (boidCPUs[i].y == y) {
+				affectedBoidCPUEdgeChanges |= ((int16(stepChanges) << NORTH_IDX));
+				std::cout << "NORTH edge decreased, ";
+			}
+		}
+
+		// If the SOUTH edge of the overloaded BoidCPU is changing and this
+		// BoidCPU is on the row below the overloaded one, raise this
+		// BoidCPU's NORTH edge. If on same row raise its SOUTH edge.
+		if (int4(edgeChanges >> SOUTH_IDX) != 0) {
+			if (boidCPUs[i].y == (y + 1)) {
+				affectedBoidCPUEdgeChanges |= ((int16(-stepChanges) << NORTH_IDX));
+				std::cout << "NORTH edge increased, ";
+			} else if (boidCPUs[i].y == y) {
+				affectedBoidCPUEdgeChanges |= ((~(int16)0xFF00) &
+						(int16(-stepChanges) << SOUTH_IDX));
+				std::cout << "SOUTH edge decreased, ";
+			}
+		}
+
+		// If the EAST edge of the overloaded BoidCPU is changing and this
+		// BoidCPU is on the column to the right of the overloaded one, widen
+		// this BoidCPU's WEST edge. If on same column narrow its EAST edge.
+		if (int4(edgeChanges >> EAST_IDX) != 0) {
+			if (boidCPUs[i].x == (x + 1)) {
+				affectedBoidCPUEdgeChanges |= ((~(int16)0xFFF0) &
+						(int16(-stepChanges) << WEST_IDX));
+				std::cout << "WEST edge increased, ";
+			} else if (boidCPUs[i].x == x) {
+				affectedBoidCPUEdgeChanges |= ((~(int16)0xF000) &
+						(int16(-stepChanges) << EAST_IDX));
+				std::cout << "EAST edge decreased, ";
+			}
+		}
+
+		// If the WEST edge of the overloaded BoidCPU is changing and this
+		// BoidCPU is on the column to the left of the overloaded one, widen
+		// this BoidCPU's EAST edge. If on same column narrow its WEST edge.
+		if (int4(edgeChanges >> WEST_IDX) != 0) {
+			if (boidCPUs[i].x == (x - 1)) {
+				affectedBoidCPUEdgeChanges |= ((~(int16)0xF000) &
+						(int16(stepChanges) << EAST_IDX));
+				std::cout << "EAST edge increased, ";
+			} else if (boidCPUs[i].x == x) {
+				affectedBoidCPUEdgeChanges |= ((~(int16)0xFFF0) &
+						(int16(stepChanges) << WEST_IDX));
+				std::cout << "WEST edge decreased, ";
+			}
+		}
+
+		std::cout << " [" << int4(affectedBoidCPUEdgeChanges >> NORTH_IDX) <<
+				", " << int4(affectedBoidCPUEdgeChanges >> EAST_IDX) << ", "
+				<< int4(affectedBoidCPUEdgeChanges >> SOUTH_IDX) << ", "
+				<< int4(affectedBoidCPUEdgeChanges >> WEST_IDX) << "]"
+				<< std::endl;
+
+		if (affectedBoidCPUEdgeChanges) {
+			data[0] = (uint32)affectedBoidCPUEdgeChanges;
+			createCommand(1, boidCPUs[i].boidCPUID, CONTROLLER_ID, CMD_LOAD_BAL, data);
+
+			// Clear ACK for gatekeeper owning BoidCPU
+			for (int j = 0; j < gatekeeperCount; j++) {
+				if (boidCPUs[i].gatekeeperID == ackList[j].gatekeeperID) {
+					ackList[j].recieved = false;
+					ackList[j].loadBalancing = true;
+					if (ackCount) ackCount--;
+
+					for (int i = 0; i < gatekeeperCount; i++) {
+						std::cout << ackList[i].gatekeeperID << ": (" << ackList[i].recieved << ", " << ackList[i].loadBalancing << ") , ";
+					} std::cout << "(" << ackCount << ")" << std::endl;
+
+					break;
+				}
+			}
+		}
+	}
+}
+
+/**
+ * When a BoidCPU reports that it is minimal, update the BoidMaster's knowledge
+ * of the BoidCPU. This is used in load balancing when determining the bound
+ * changes to do to balance the load.
+ *
+ * 0 = minimal width, 1 = minimal height, 2 = minimal height and width
+ */
+void updateMinimalBoidCPUsList() {
+	if (inputData[CMD_HEADER_LEN]) {
+		boidCPUs[inputData[CMD_FROM] - FIRST_BOIDCPU_ID].minimalWidth = true;
+		std::cout << "BoidCPU #" << CMD_FROM << " at minimal width" << std::endl;
+	} else if (inputData[CMD_HEADER_LEN] == 1) {
+		boidCPUs[inputData[CMD_FROM] - FIRST_BOIDCPU_ID].minimalHeight = true;
+		std::cout << "BoidCPU #" << CMD_FROM << " at minimal height" << std::endl;
+	} else {
+		boidCPUs[inputData[CMD_FROM] - FIRST_BOIDCPU_ID].minimalHeight = true;
+		boidCPUs[inputData[CMD_FROM] - FIRST_BOIDCPU_ID].minimalWidth = true;
+		std::cout << "BoidCPU #" << CMD_FROM << " at minimum" << std::endl;
+ 	}
+ }
+#endif
 
 //============================================================================//
 // Outgoing functions --------------------------------------------------------//
@@ -439,6 +690,14 @@ void issueCalcBoidMode() {
 	dataLength = 0;
 	createCommand(dataLength, to, from, MODE_POS_BOIDS, data);
 }
+
+#ifdef LOAD_BALANCING_ENABLED
+void issueLoadBalance() {
+	to = CMD_BROADCAST;
+	dataLength = 0;
+	createCommand(dataLength, to, from, MODE_LOAD_BAL, data);
+}
+#endif
 
 void issueTransferMode() {
 	to = CMD_BROADCAST;
